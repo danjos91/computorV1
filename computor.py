7108b9dff08edbd316dfc1716c4a376107e166f2
Danny Tituaña <davila.tituanya@gazprombank.ru>
2022-06-05 21:50:21 +0300

    done

--- computor.py
+++ computor.py
@@ -1,18 +1,10 @@
 import sys
-import ctypes
 
-equation = "5 * X^0 + 4 * X^1 - 9.367 * X^2 = 1 * X^0 + 1 * X^2"
 
 class EcuationMember:
     def __init__(self, value, category):
         self.value = value
         self.category = category
-
-
-class Complex:
-    def __init__(self, realpart, imagpart):
-        self.real = realpart
-        self.imaginary = imagpart
 
 
 def get_coeff(equation_member, exponent):
@@ -56,7 +48,7 @@ def get_degree(equation):
         if degree_value > tmp_degree:
             tmp_degree = degree_value
         position = equation.find("^", degree_position)
-    return degree_value
+    return tmp_degree
 
 
 def supress_operators(elements):
@@ -78,7 +70,7 @@ def add_category(elements):
             elements.append((0, i))
 
 
-def print_equation(equation):
+def print_reduced_equation(equation):
     text = ''
     for i in range(3):
         value = equation[i][0]
@@ -90,6 +82,8 @@ def print_equation(equation):
                     text += "+ " + str(value) + " * X^" + str(i) + " "
             else:
                 text += "- " + str(value * (-1)) + " * X^" + str(i) + " "
+    if text == '':
+        return "0 = 0"
     text += "= 0"
     return text
 
@@ -99,7 +93,6 @@ def do_equal_to_zero(equation):
     elements_before_equal = []
     equal = False
     value = 0
-    category = 1
     for element in equation:
         if element[value] == '=':
             equal = True
@@ -148,62 +141,137 @@ def get_equation_members(equation):
             try:
                 float(element)
             except:
-                print("Not valid equation...Please check it.")
+                print("Error found in: " + element)
                 sys.exit(-1)
         counter += 1
     return elements_list
 
 
-def solve_equation(reduced_form):
+def print_solved_equation(equation, reduced_equation, solutions, discriminant, degree):
     # Ecuation form ax^2 + bx + c = 0
-    a = reduced_form[2][0]
-    b = reduced_form[1][0]
-    c = reduced_form[0][0]
-    discriminant = b ** 2 - 4 * a * c
+    a = reduced_equation[2][0]
+    b = reduced_equation[1][0]
+    c = reduced_equation[0][0]
+
+    print("Equation:")
+    print("\t" + equation)
+    print("Reduced equation:")
+    print("\t" + print_reduced_equation(reduced_equation))
 
     if discriminant > 0:
         print('Discriminant is strictly positive')
-        print("Solutions: ")
-        x1 = (b * (-1) + discriminant ** 0.5) / (2 * a)
-        print("X1 = " + str(x1))
-        x2 = (b * (-1) - discriminant ** 0.5) / (2 * a)
-        print("X2 = " + str(x2))
+    if discriminant < 0:
+        print('Discriminant is strictly negative')
+    if degree == 1:
+        type = "c + bx = 0"
+        print("Solution:")
+        x1 = solutions[0]
+        print("\tX = " + str(x1))
+        print()
         print("STEPS:")
-        print("\tWe have an equation of type: c + bx + ax^2 = 0")
-        print("\t" + print_equation(reduced_form))
+        print("\tWe have an equation of type: %s" % type)
+        print("\t" + print_reduced_equation(reduced_equation))
+        print("\t(" + str(b) + ")*(X) = " + str(-1 * c))
+        print("\tX = " + str(-1 * c) + "/" + str(b))
+        print("\tX = " + str(-1 * c / b))
+        print()
+        print("Discriminant:\n   D = b^2 - 4ac")
+        print('\tDiscriminant = (' + str(b) + ")^2 - 4*(" + str(a) + ")*(" + str(c) + ") = " + str(discriminant))
+    elif degree == 2:
+        type = "c + bx + ax^2 = 0"
+        print("Solutions: ")
+        x1 = solutions[0]
+        print("X1 = " + str(x1))
+        x2 = solutions[1]
+        print("X2 = " + str(x2))
+        print()
+        print("STEPS:")
+        print("\tWe have an equation of type: %s" % type)
+        print("\t" + print_reduced_equation(reduced_equation))
+        print()
         print("1) Found the discriminant:\n   D = b^2 - 4ac")
         print('\tDiscriminant = (' + str(b) + ")^2 - 4*(" + str(a) + ")*(" + str(c) + ") = " + str(discriminant))
-        print("2) If D > 0:\n   X[1,2] = (-b +/- sqrt(b^2 - 4ac)) / 2a")
-        print("\tX1 = (-(" + str(b) + ") + sqrt((" + str(b) + ")^2 - 4*( " + str(a) + ")*(" + str(c) + "))) / (2*(" + str(a) + "))")
+        print()
+        print("2) D > 0:\n   X[1,2] = (-b +- sqrt(b^2 - 4ac)) / 2a")
+        print()
+        print(
+            "\tX1 = (-(" + str(b) + ") + sqrt((" + str(b) + ")^2 - 4*( " + str(a) + ")*(" + str(c) + "))) / (2*(" + str(
+                a) + "))")
         print("\tX1 = (" + str(b * (-1)) + ") + sqrt(" + str(discriminant) + ")) / (2*(" + str(a) + "))")
-        print("\tX1 = (" + str(b * (-1)) + " + " + str(discriminant ** 0.5) + ") / " + str(2*a))
-        print("\tX1 = " + str(b * (-1) + discriminant ** 0.5) + " / " + str(2 * a))
+        if discriminant < 0:
+            print("\tX1 = (" + str(b * (-1)) + " + " + round_eq((discriminant ** 0.5).imag) + "i) / " + str(2 * a))
+        else:
+            print("\tX1 = (" + str(b * (-1)) + " + " + str(discriminant ** 0.5) + ") / " + str(2 * a))
+            print("\tX1 = " + str(b * (-1) + discriminant ** 0.5) + " / " + str(2 * a))
         print("\tX1 = " + str(x1))
-        print("\tX2 = (-(" + str(b) + ") - sqrt((" + str(b) + ")^2 - 4*( " + str(a) + ")*(" + str(c) + "))) / (2*(" + str(a) + "))")
+        print()
+        print(
+            "\tX2 = (-(" + str(b) + ") - sqrt((" + str(b) + ")^2 - 4*( " + str(a) + ")*(" + str(c) + "))) / (2*(" + str(
+                a) + "))")
         print("\tX2 = (" + str(b * (-1)) + " - sqrt(" + str(discriminant) + ")) / (2*(" + str(a) + "))")
-        print("\tX2 = " + str(b * (-1) - discriminant ** 0.5) + " / " + str(2 * a))
+        if discriminant < 0:
+            print("\tX2 = (" + str(b * (-1)) + " - " + round_eq((discriminant ** 0.5).real) + ") / " + str(2 * a))
+        else:
+            print("\tX2 = (" + str(b * (-1)) + " - " + str(discriminant ** 0.5) + ") / " + str(2 * a))
+            print("\tX2 = " + str(b * (-1) - discriminant ** 0.5) + " / " + str(2 * a))
         print("\tX2 = " + str(x2))
+        print()
+    print("Polynomial degree: ", degree)
+
 
 def solve(equation):
     degree = get_degree(equation)
     if degree > 2:
-        print("I cant solve second degree equations only")
+        print("Sorry. I can solve second degree equations only")
         return False
     equation_elements = get_equation_members(equation)
     reduced_equation = do_equal_to_zero(equation_elements)
-    print(print_equation(reduced_equation))
-    solve_equation(reduced_equation)
-    print("Polynomial degree: ", degree)
 
+    a = reduced_equation[2][0]
+    b = reduced_equation[1][0]
+    c = reduced_equation[0][0]
+
+    discriminant = b ** 2 - 4 * a * c
+
+    solutions = []
+    if discriminant > 0:
+        if degree == 0:
+            if c != 0:
+                exit(-1)
+            else:
+                solutions.append(0)
+        if degree == 1:
+            x1 = (-1) * c / b
+            solutions.append(x1)
+        elif degree == 2:
+            x1 = (b * (-1) + discriminant ** 0.5) / (2 * a)
+            x2 = (b * (-1) - discriminant ** 0.5) / (2 * a)
+            solutions.append(x1)
+            solutions.append(x2)
+    elif discriminant < 0:
+            x1 = (b * (-1) + discriminant ** 0.5) / (2 * a)
+            x1 = round_eq(x1.real) + " + (" + round_eq(x1.imag) + ")i"
+            x2 = (b * (-1) - discriminant ** 0.5) / (2 * a)
+            x2 = round_eq(x2.real) + " + (" + round_eq(x2.imag) + ")i"
+            solutions.append(x1)
+            solutions.append(x2)
+
+    print_solved_equation(equation, reduced_equation, solutions, discriminant, degree)
+
+def round_eq(value):
+    return str(round(value, 2))
 
 def main():
     if len(sys.argv) == 2:
-        equation = sys.argv[1]
-        if not validate(equation):
+        try:
+            equation = sys.argv[1]
+            if not validate(equation):
+                return
+            if not solve(equation):
+                return
+        except:
             print("Not valid equation...Please check it.")
-            return
-        if not solve(equation):
-            return
+
     else:
         print(
             'Write an equation as an argument.\nUsage example: python3 computor.py "5 * X^0 + 4 * X^1 - 9.367 * X^2 = 1 * '
